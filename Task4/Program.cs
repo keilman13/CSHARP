dcdd2b0ea2faf0798b2e6ad7b8ca3595e0d21aa5
keilman13 <163523080+keilman13@users.noreply.github.com>
2024-04-11 21:28:47 +0300

    add lesson:task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 606;
+int b = 77;
+int c = 89;
+int d = 94;
+int e = 100;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
